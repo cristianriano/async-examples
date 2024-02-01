777687b357b80eb1fc33e46d14849a9524e396a7
Cristian Riano <cristiancarm11@gmail.com>
2024-02-01 23:15:22 +0100

    Clean gradle.properties

--- lib/src/test/kotlin/com/example/HelloWorldTest.kt
+++ lib/src/test/kotlin/com/example/HelloWorldTest.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class HelloWorldTest {
-  
+
   @Test
   fun someLibraryMethodReturnsTrue() {
     val classUnderTest = HelloWorld()
