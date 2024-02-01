d1ced641b65a73f53b03d38e1fd5ce310856add0
Cristian Riano <cristiancarm11@gmail.com>
2024-02-01 23:59:07 +0100

    Add Spotless plugin

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
   // Need to specify version unless it comes from buildSrc lib
   alias(libs.plugins.kotlin.gradle)
+  alias(libs.plugins.spotless.gradle)
 
   `java-library`
 }
@@ -35,4 +36,26 @@ tasks.withType<KotlinCompile> {
 java {
   sourceCompatibility = JavaVersion.VERSION_17
   targetCompatibility = JavaVersion.VERSION_17
+}
+
+spotless {
+  java {
+    apply {
+      target("src/**/*.java")
+    }
+
+    endWithNewline()
+    indentWithSpaces()
+    googleJavaFormat()
+    removeUnusedImports()
+    toggleOffOn()
+    trimTrailingWhitespace()
+  }
+
+  kotlin {
+    apply {
+      target("src/**/*.kt")
+      ktfmt().googleStyle()
+    }
+  }
 }
