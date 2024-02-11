5ac4c612cdf05f4a27c4fecac028295570bb190c
Cristian Riano <cristiancarm11@gmail.com>
2024-02-11 13:47:54 +0100

    Disable CI flacky test

--- lib/src/test/kotlin/com/example/ThreadInterferenceExamplesTest.kt
+++ lib/src/test/kotlin/com/example/ThreadInterferenceExamplesTest.kt
@@ -1,6 +1,7 @@
 package com.example
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class ThreadInterferenceExamplesTest {
@@ -8,6 +9,7 @@ class ThreadInterferenceExamplesTest {
   private val examples = ThreadInterferenceExamples()
 
   @Test
+  @Disabled("In CI can't trigger the race condition")
   fun `when concurrent and without sync it sums different than 0`() {
     val sum = (1..5).sumOf { examples.runUnSync() }
 
